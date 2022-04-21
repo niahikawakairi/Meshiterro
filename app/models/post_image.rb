fcea35e1bae92ad70f276c0a178dab7e545a97d0
niahikawakairi <kairi.0109@au.com>
2022-04-21 13:15:56 +0000

    update3

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -2,7 +2,8 @@ class PostImage < ApplicationRecord
 
   has_one_attached :image
   belongs_to :user
-  
+  has_many :post_comments, dependent: :destroy
+
   def get_image
     if image.attached?
       image
@@ -10,7 +11,7 @@ class PostImage < ApplicationRecord
       'no_image.jpg'
     end
   end
-  
+
   def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
